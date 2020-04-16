03e820af8b03524491931f3cd96995bcf784dc83
Alberto Dragoni <albertodragoni@yahoo.it>
2020-04-16 16:33:10 +0200

    esercizio ok

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
-  echo ('ciao');
+
+  $password = $_GET['password'];
+  echo $password;
+  if ($password == 'Boolean') {
+    echo "<h1 style='color:green;'> PUOI ACCEDERE </h1>";
+  } else {
+    echo "<h1 style='color:red;'> NON PUOI ACCEDERE </h1>";
+  };
 
  ?>
